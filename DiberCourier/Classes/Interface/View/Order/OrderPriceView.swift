b9e2ee36b30534b001b6597950ab81a3daaff185
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2017-12-03 22:16:15 +0300

    Order Details implementation. Added request call.

---
+++ DiberCourier/Classes/Interface/View/Order/OrderPriceView.swift
@@ -0,0 +1,9 @@
+//
+//  OrderPriceView.swift
+//  DiberCourier
+//
+//  Created by Alexander Tereshkov on 12/3/17.
+//  Copyright © 2017 Diber. All rights reserved.
+//
+
+import Foundation
---
+++ DiberCourier/Classes/Interface/View/Order/OrderRequestView.swift
@@ -0,0 +1,16 @@
+//
+//  OrderRequestView.swift
+//  DiberCourier
+//
+//  Created by Alexander Tereshkov on 12/3/17.
+//  Copyright © 2017 Diber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OrderRequestView: UIView {
+    
+    
+    
+}
