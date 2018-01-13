68a606b5a83de4317290591e3ab82ca72586e916
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-01-13 13:35:47 +0300

    Initial Signup Storyboard added

---
+++ DiberCourier/Classes/Controllers/SignupVC.swift
@@ -0,0 +1,38 @@
+//
+//  SignupVC.swift
+//  DiberCourier
+//
+//  Created by Alexander Tereshkov on 1/13/18.
+//  Copyright © 2018 Diber. All rights reserved.
+//
+
+import Foundation
+import MBProgressHUD
+
+class SignupVC: UIViewController {
+    
+    
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        LogManager.log.info("Deinitialization")
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK:
+    
+    private func perfomSignup() {
+        
+    }
+    
+}
