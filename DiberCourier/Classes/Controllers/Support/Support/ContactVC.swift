23eab08f631ae42a7bfd4f24e826dee9eb2307f3
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-04-08 19:24:19 +0300

    Base "Contact Us" VC

---
+++ DiberCourier/Classes/Controllers/Support/Support/ContactVC.swift
@@ -0,0 +1,31 @@
+//
+//  ContactVC.swift
+//  DiberCourier
+//
+//  Created by Alexander Tereshkov on 4/8/18.
+//  Copyright © 2018 Diber. All rights reserved.
+//
+
+import UIKit
+
+class ContactVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        LogManager.log.info("Deinit")
+    }
+    
+    // MARK: Private
+    
+    
+    
+    // MARK: Actions
+    
+    @IBAction func backDidPressed(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
