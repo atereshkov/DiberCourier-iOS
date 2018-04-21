dd2e4ad7db56d0563dc7cedc7806d29b6a6c0d8d
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-04-22 01:02:18 +0300

    New colors for messages

--- DiberCourier/Classes/Interface/View/Messages/MessageBubbleView.swift
+++ DiberCourier/Classes/Interface/View/Messages/MessageBubbleView.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MessageBubbleView: UIView {
     
+    let myColor = UIColor.init(red: 154.0 / 255.0, green: 211.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+    let senderColor = UIColor.init(red: 201.0 / 255.0, green: 224.0 / 255.0, blue: 191.0 / 255.0, alpha: 1.0)
+    
     var currentUserIsSender = true {
         didSet {
             setNeedsDisplay()
@@ -18,23 +21,23 @@ class MessageBubbleView: UIView {
     
     override func draw(_ rect: CGRect) {
         let bezierPath = UIBezierPath()
-        //Draw main body
+        // draw main body
         bezierPath.move(to: CGPoint(x: rect.minX, y: rect.minY))
         bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
         bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - 10.0))
         bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - 10.0))
         bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        //Draw the tail
+        // draw the tail
         if currentUserIsSender {
             bezierPath.move(to: CGPoint(x: rect.maxX - 25.0, y: rect.maxY - 10.0))
             bezierPath.addLine(to: CGPoint(x: rect.maxX - 10.0, y: rect.maxY))
             bezierPath.addLine(to: CGPoint(x: rect.maxX - 10.0, y: rect.maxY - 10.0))
-            UIColor.lightGray.setFill()
+            myColor.setFill()
         } else {
             bezierPath.move(to: CGPoint(x: rect.minX + 25.0, y: rect.maxY - 10.0))
             bezierPath.addLine(to: CGPoint(x: rect.minX + 10.0, y: rect.maxY))
             bezierPath.addLine(to: CGPoint(x: rect.minX + 10.0, y: rect.maxY - 10.0))
-            UIColor.orange.setFill()
+            senderColor.setFill()
         }
         bezierPath.fill()
         bezierPath.close()
